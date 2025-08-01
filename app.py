0ab48e540521514a71db78f166dd5bc43c6e8de0
ms001 <aquibms1234@gmail.com>
2025-08-01 12:23:32 +0530

    2nd change

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask 
+from flask import Flask ,render_template
 
 app = Flask (__name__)
 @app.route("/")
 
     
 def hello():
-    name = 'mukhtar'
-    return f"\t\t Hello\t{name} !!!!!!!! "
+    
+    return render_template("index.html", name = "mukhtar".upper())
 
 if __name__ ==('__main__'):
     app.run(host= '0.0.0.0' ,debug=True)
