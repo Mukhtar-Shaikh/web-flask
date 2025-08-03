dd90a29cf878361dfe078edd528baaea88022c36
ms001 <aquibms1234@gmail.com>
2025-08-03 10:25:37 +0530

    10.30 ,3 august

--- app.py
+++ app.py
@@ -1,29 +1,38 @@
-from flask import Flask ,render_template
+from flask import Flask ,render_template ,jsonify
 
 app = Flask (__name__)
-
-@app.route("/")
-
-def hello():
-    work = [
+work = [
     {
-        "id" : 1,
+        "location" : "pune",
         "role" :"backend",
         "salary" :2400000
     },
     {
-        "id" : 2,
+        "location" : "pune",
         "role" :"front-end",
         "salary" :2000000
     },
     {
-        "id" : 1,
-        "role" :"ml",
+        "location" : "mumbai",
+        "role" :"ml engineer ",
         "salary" :2500000
+    },
+    {
+        "location" : "mumbai",
+        "role" :"Intern "
+        
     }
 ]
+
+@app.route("/")
+
+def hello():
+    
     
     return render_template("index.html", name = "mukhtar".upper(), jobs=work)
+@app.route("/api/jobs")
+def jobs():
+    return jsonify(work)
 
 if __name__ ==('__main__'):
     app.run(host= '0.0.0.0' ,debug=True)
